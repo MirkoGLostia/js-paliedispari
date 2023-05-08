c23961b1ec27a3de0a0cae73e54bf16aab45469b
MirkoGLostia <giggi.torre@mail.com>
2023-05-08 17:09:35 +0200

    js_logica-per-palindromo

--- palindromi/js/palimain.js
+++ palindromi/js/palimain.js
@@ -4,6 +4,34 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire
 */
 
+// contenitore parola al contrarioi vuota
+let reverseWord = "";
 
 
 
+// chiedere parola utente
+const parolaUtente = prompt("la tua parola");
+
+
+
+// ciclo per invertire parola
+for (let i = (parolaUtente.length - 1); i >= 0; i--) {
+    
+    const lettere = parolaUtente[i];
+
+    reverseWord += lettere;
+    
+}
+
+
+// confronto parola utente e reverse
+if (parolaUtente === reverseWord) {
+    
+    console.log("palindromo");
+
+}
+else {
+
+    console.log("non palindromo");
+
+}
