26f150d936c15055d6645a3f47e4c7968298ead6
MirkoGLostia <giggi.torre@mail.com>
2023-05-08 17:22:13 +0200

    pali_js_creazione-funzione

--- palindromi/js/palimain.js
+++ palindromi/js/palimain.js
@@ -5,7 +5,7 @@ Creare una funzione per capire
 */
 
 // contenitore parola al contrarioi vuota
-let reverseWord = "";
+let reverseParolaUtente = "";
 
 
 
@@ -14,24 +14,43 @@ const parolaUtente = prompt("la tua parola");
 
 
 
-// ciclo per invertire parola
-for (let i = (parolaUtente.length - 1); i >= 0; i--) {
-    
-    const lettere = parolaUtente[i];
+// risultato della funzione
+let wordCheck = palindromeCheck(parolaUtente, reverseParolaUtente);
 
-    reverseWord += lettere;
+
+
+
+// functions
+function palindromeCheck (word, reverse) {
+
+    let result = "non palindromo";
+
+    // ciclo per invertire parola
+    for (let i = (word.length - 1); i >= 0; i--) {
+        
+        const lettere = word[i];
     
+        reverse += lettere;
+        
+    }
+    
+    // confronto parola utente e reverse
+    if (word === reverse) {
+        
+        result = "palindromo";
+
+        console.log("palindromo");
+    
+    }
+    else {
+    
+        console.log("non palindromo");
+    
+    }
+
+    return result
+
 }
 
 
-// confronto parola utente e reverse
-if (parolaUtente === reverseWord) {
-    
-    console.log("palindromo");
 
-}
-else {
-
-    console.log("non palindromo");
-
-}
